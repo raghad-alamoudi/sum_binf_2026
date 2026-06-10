2c2a5d04b9c71a2f6ad34403364f2b2aa0d387a3
Raghad Alamoudi <ralamodi.salah@gmail.com>
2026-06-10 19:06:47 -0400

    Rosalind INI2 & INI3 solution

--- rosiland/python_village.py
+++ rosiland/python_village.py
@@ -1,4 +1,7 @@
-# INI2
+# INI2 
+'''Given: Two positive integers aand b, each less than 1000.
+Return: The integer corresponding to the square of the hypotenuse of the 
+right triangle whose legs have lengths a and b.'''
 def square_hypotenuse(a, b):
     c = (a ** 2) + (b ** 2)
     return c    
